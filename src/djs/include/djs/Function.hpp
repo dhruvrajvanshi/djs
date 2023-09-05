bff6ec018bacfb4c5e2c02f20035303b9d6bc624
Dhruv Rajvanshi <d@rajv.dev>
2023-09-05 12:59:43 +0200

    Make BasicBlock and Function copyable

--- src/djs/include/djs/Function.hpp
+++ src/djs/include/djs/Function.hpp
@@ -9,11 +9,9 @@ namespace djs {
 
 struct BasicBlock {
   const std::vector<Instruction> instructions;
-  NOCOPY(BasicBlock)
 };
 struct Function {
   const std::vector<BasicBlock> basic_blocks;
-  NOCOPY(Function)
 };
 
 }; // namespace djs
