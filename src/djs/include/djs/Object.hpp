21615150bf9b04729a10807b1f021e7ea92a742d
Dhruv Rajvanshi <d@rajv.dev>
2023-09-05 19:36:36 +0200

    Add Object type

---
+++ src/djs/include/djs/Object.hpp
@@ -0,0 +1,3 @@
+namespace djs {
+struct Object {};
+} // namespace djs
--- src/djs/include/djs/Value.hpp
+++ src/djs/include/djs/Value.hpp
@@ -12,8 +12,11 @@ enum class ValueType {
   Undefined,
   Null,
   NativeFunction,
+  Object,
 };
 using NativeFunction = CompletionRecord (*)(VM &, std::span<Value>);
+
+class Object;
 
 class Value {
 
@@ -25,6 +28,7 @@ private:
     uint8_t undefined;
     uint8_t null;
     NativeFunction native_function;
+    Object *object;
   } as;
   Value(Type type, As as) : type(type), as(as) {}
 
@@ -34,6 +38,11 @@ public:
 
   static auto null() -> Value;
   auto is_null() const -> bool;
+
+  static auto object(Object *obj) -> Value {
+    return {Type::Object, {.object = obj}};
+  }
+  auto is_object() -> bool { return type == Type::Object; }
 
   static auto native_function(NativeFunction f) -> Value {
     return Value{Type::NativeFunction, {.native_function = f}};
