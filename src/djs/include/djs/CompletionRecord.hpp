33f74c2a89435e4a780ddfbe4f351a9e01da708e
Dhruv Rajvanshi <d@rajv.dev>
2023-09-05 12:46:55 +0200

    Reformat

--- src/djs/include/djs/CompletionRecord.hpp
+++ src/djs/include/djs/CompletionRecord.hpp
@@ -3,19 +3,13 @@
 #include <optional>
 
 namespace djs {
-    struct CompletionRecord {
-        enum class Kind {
-            Normal,
-            Return,
-            Throw,
-            Break,
-            Continue
-        };
-        const Kind kind;
-        const std::optional<Value> value;
+struct CompletionRecord {
+  enum class Kind { Normal, Return, Throw, Break, Continue };
+  const Kind kind;
+  const std::optional<Value> value;
 
-        static auto normal(Value value) -> CompletionRecord {
-            return { Kind::Normal, value };
-        }
-    };
+  static auto normal(Value value) -> CompletionRecord {
+    return {Kind::Normal, value};
+  }
+};
 } // namespace djs
