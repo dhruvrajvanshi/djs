73491819987c0b733176c90754d849f1d4cd7102
Dhruv Rajvanshi <d@rajv.dev>
2023-09-05 14:45:00 +0200

    Add panic and assert

--- src/djs/include/djs/Common.hpp
+++ src/djs/include/djs/Common.hpp
@@ -1,5 +1,25 @@
 #pragma once
+#include <iostream>
 
 #define NOCOPY(T)                                                              \
   T(const T &) = delete;                                                       \
   T &operator=(T) = delete;
+
+namespace djs {
+
+template <typename T>
+concept Printable = requires(T t, std::ostream s) {
+  { (s << t) };
+};
+
+template <typename T, Printable M> auto panic(M s) -> T {
+  std::cerr << "PANIC: " << s << std::endl;
+  std::abort();
+}
+
+template <typename T, Printable M> auto assert(bool condition, M message) -> T {
+  if (condition) {
+    return panic<T>(message);
+  }
+}
+} // namespace djs
