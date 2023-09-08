b6e3bfe74443db5ef98e08aee905707a59b5194d
Dhruv Rajvanshi <d@rajv.dev>
2023-09-08 13:29:01 +0200

    Make function an Object

--- src/djs/include/djs/Function.hpp
+++ src/djs/include/djs/Function.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "./Common.hpp"
 #include "./Instruction.hpp"
+#include "./Object.hpp"
 
 #include <string>
 #include <vector>
@@ -10,9 +11,18 @@ namespace djs {
 struct BasicBlock {
   Vec<Instruction> instructions;
 };
-struct Function {
+struct Function : Object {
   Vec<BasicBlock> basic_blocks;
   size_t local_count;
+  Function(Vec<BasicBlock> blocks, size_t local_count)
+      : Object(function_slots()), basic_blocks(std::move(blocks)),
+        local_count(local_count) {}
+
+private:
+  static auto function_slots() -> Slots {
+    auto slots = Slots::ordinary();
+    return slots;
+  }
 };
 
 }; // namespace djs
