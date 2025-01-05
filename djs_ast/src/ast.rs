1d4cece8f72a507a281ef6903d803f0aca1e6a57
Dhruv Rajvanshi <dhruv@softr.io>
2025-01-05 16:19:28 +0100

    Add AST nodes for arrow functions

--- djs_ast/src/ast.rs
+++ djs_ast/src/ast.rs
@@ -6,7 +6,7 @@ pub struct Stmt<'src> {
 }
 pub enum StmtKind<'src> {
     Expr(Box<Expr<'src>>),
-    Block(Vec<Stmt<'src>>),
+    Block(Block<'src>),
     Return(Option<Box<Expr<'src>>>),
 }
 
@@ -18,7 +18,27 @@ pub struct Expr<'src> {
 pub enum ExprKind<'src> {
     Var(&'src str),
     BinOp(Box<Expr<'src>>, BinOp, Box<Expr<'src>>),
+    ArrowFn(ParamList<'src>, ExprOrBlock<'src>),
 }
+
+pub struct ParamList<'src> {
+    pub span: Span,
+    pub params: Vec<Param<'src>>,
+}
+pub enum ExprOrBlock<'src> {
+    Expr(Box<Expr<'src>>),
+    Block(Block<'src>),
+}
+pub struct Block<'src> {
+    pub span: Span,
+    pub stmts: Vec<Stmt<'src>>,
+}
+
+pub struct Param<'src> {
+    pub span: Span,
+    pub name: &'src str,
+}
+
 pub enum BinOp {
     Add,
     Sub,
