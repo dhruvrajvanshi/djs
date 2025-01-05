3b735aa977b5af22984b0e4fa555e6a9fb30c040
Dhruv Rajvanshi <dhruv@softr.io>
2025-01-05 12:38:27 +0100

    Add lexer implementation

--- djs_syntax/src/lib.rs
+++ djs_syntax/src/lib.rs
@@ -1,4 +1,13 @@
+#[derive(Debug, Copy, Clone)]
 pub struct Span {
     pub start: u32,
     pub end: u32,
+}
+impl Span {
+    pub fn start(&self) -> usize {
+        self.start as usize
+    }
+    pub fn end(&self) -> usize {
+        self.end as usize
+    }
 }
