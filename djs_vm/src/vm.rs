339da323299ffef5d4f6c5ea467c96eb01b3576e
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-01-14 16:01:31 +0100

    Functions with no args

--- djs_vm/src/vm.rs
+++ djs_vm/src/vm.rs
@@ -1,46 +1,75 @@
-use std::ops::{Index, IndexMut};
-
 use crate::{
-    bytecode::{Bytecode, Register, REG_RETURN},
-    value::{Function, Value},
+    bytecode::Bytecode,
+    value::{Function, ObjectKind, Value},
 };
 
-pub struct VM {
-    registers: Registers,
-}
-
-struct Registers([Value; 256]);
-impl Index<Register> for Registers {
-    type Output = Value;
-
-    fn index(&self, index: Register) -> &Self::Output {
-        &self.0[index as usize]
-    }
-}
-impl IndexMut<Register> for Registers {
-    fn index_mut(&mut self, index: Register) -> &mut Self::Output {
-        &mut self.0[index as usize]
-    }
+pub struct VM;
+struct CallFrame<'a> {
+    return_address: Option<usize>,
+    function: &'a Function,
 }
 
 impl VM {
     pub fn new() -> Self {
-        VM {
-            registers: Registers([Value::Undefined; 256]),
-        }
+        VM
     }
 
-    pub fn call(&mut self, f: &Function) -> Value {
+    pub fn run(&mut self, code: &[Bytecode]) -> Value {
         let mut ip = 0;
+        let mut stack: Vec<Value> = Vec::with_capacity(1024);
+        let mut call_stack: Vec<CallFrame> = Vec::with_capacity(1024);
+        let function = Function {
+            code: code.to_vec(),
+        };
+        call_stack.push(CallFrame {
+            return_address: None,
+            function: &function,
+        });
         loop {
-            let instruction = f.code[ip];
+            let instruction = call_stack.last().unwrap().function.code[ip];
             use Bytecode::*;
             match instruction {
-                LoadConst(reg, value) => {
-                    self.registers[reg] = value;
+                LoadConst(value) => {
+                    stack.push(value);
                     ip += 1;
                 }
-                Return => return self.registers[REG_RETURN],
+                Return => {
+                    let result = call_stack.pop();
+                    match result {
+                        Some(CallFrame {
+                            return_address: Some(return_address),
+                            ..
+                        }) => {
+                            ip = return_address;
+                        }
+                        Some(CallFrame {
+                            return_address: None,
+                            ..
+                        })
+                        | None => {
+                            return stack.pop().expect("Stack underflow");
+                        }
+                    }
+                }
+                Call => {
+                    let function = stack.pop().expect("Stack underflow");
+                    match function {
+                        Value::Object(obj) => {
+                            let obj = unsafe { &*obj.as_ptr() };
+                            let func = match &obj.kind {
+                                ObjectKind::Function(f) => f,
+                                k => panic!("Expected a function object; Found {k:?}"),
+                            };
+
+                            call_stack.push(CallFrame {
+                                return_address: Some(ip + 1),
+                                function: func,
+                            });
+                            ip = 0;
+                        }
+                        value => panic!("Expected a function object; Found {value:?}"),
+                    }
+                }
             }
         }
     }
@@ -55,8 +84,9 @@ impl Default for VM {
 #[cfg(test)]
 mod test {
     use core::f64;
+    use std::ptr::{self, NonNull};
 
-    use crate::{bytecode::REG_RETURN, value::Function, value::Value};
+    use crate::value::{Function, Object, ObjectKind, Value};
 
     use super::VM;
 
@@ -65,12 +95,23 @@ mod test {
         let mut vm = VM::new();
 
         use crate::bytecode::Bytecode as B;
-        let value = vm.call(&Function {
-            code: vec![
-                B::LoadConst(REG_RETURN, Value::Number(f64::consts::PI)),
-                B::Return,
-            ],
-        });
+        let value = vm.run(&[B::LoadConst(Value::Number(f64::consts::PI)), B::Return]);
         assert!(matches!(value, Value::Number(f64::consts::PI)));
+    }
+
+    #[test]
+    fn test_function_calls() {
+        let mut vm = VM::new();
+        use crate::bytecode::Bytecode::*;
+        let mut f2 = Object {
+            prototype: std::ptr::null_mut(),
+            properties: Default::default(),
+            kind: ObjectKind::Function(Function {
+                code: vec![LoadConst(Value::Number(2.0)), Return],
+            }),
+        };
+        let f2 = Value::Object(NonNull::new(ptr::from_mut(&mut f2)).unwrap());
+        let value = vm.run(&[LoadConst(f2), Call, Return]);
+        assert!(matches!(value, Value::Number(2.0)));
     }
 }
