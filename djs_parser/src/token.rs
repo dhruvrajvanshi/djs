b9d84be1df9edd69072f55862cb645ea7ef1262d
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-01-22 17:08:07 +0100

    Generate token kinds

--- djs_parser/src/token.rs
+++ djs_parser/src/token.rs
@@ -1,3 +1,4 @@
+pub use djs_ast::TokenKind;
 use djs_syntax::Span;
 
 #[derive(Debug, Clone, Copy)]
@@ -6,84 +7,4 @@ pub struct Token<'src> {
     pub span: Span,
     pub text: &'src str,
     pub line: u32,
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub enum TokenKind {
-    Ident,
-    Let,
-    Var,
-    Const,
-    If,
-    While,
-    Else,
-    Function,
-    Try,
-    Catch,
-    Finally,
-    Throw,
-    Return,
-    For,
-    New,
-    In,
-    Of,
-    Instanceof,
-    Yield,
-    Async,
-    Await,
-    Break,
-    Continue,
-    Debugger,
-    With,
-    Delete,
-    Typeof,
-    Void,
-    True,
-    False,
-
-    String,
-    Number,
-    Regex,
-
-    LBrace,
-    RBrace,
-    LParen,
-    RParen,
-    LSquare,
-    RSquare,
-    Comma,
-    Colon,
-    Dot,
-    Semi,
-    Question,
-    Bang,
-    Tilde,
-
-    FatArrow,
-    Eq,
-
-    EqEq,
-    EqEqEq,
-    BangEq,
-    BangEqEq,
-
-    LessThan,
-    LessThanEq,
-    GreaterThan,
-    GreaterThanEq,
-
-    Plus,
-    Minus,
-    MinusMinus,
-    PlusPlus,
-    Star,
-    Slash,
-
-    VBar,
-    VBarVBar,
-    Amp,
-    AmpAmp,
-    Caret,
-
-    EndOfFile,
 }
