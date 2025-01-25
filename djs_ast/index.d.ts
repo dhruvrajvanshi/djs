c9271949e9e2822ccba68c748d2ff0f1e958e743
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-01-25 20:31:42 +0100

    Re-add spans to expressions with operators at the start/end

--- djs_ast/index.d.ts
+++ djs_ast/index.d.ts
@@ -2,7 +2,7 @@
 type Type1 = ["Vec" | "Box" | "Option", Type];
 type Type = string | Type1;
 type Tag = "span";
-type EnumVariant = { name: string, args: Type[] };
+type EnumVariant = { name: string, args: Type[], tags: Tag[] };
 type EnumItem = { kind: "enum", name: string, tags: Tag[], variants: EnumVariant[] }
 type StructItem = { kind: "struct", name: string, tags: Tag[], fields: [string, Type][] }
 type Item = EnumItem | StructItem;
