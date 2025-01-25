07a049d6a2609e2208eedd6c663a2f990ec3b28c
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-01-25 16:05:25 +0100

    Parse getter and setter methods

--- djs_parser/src/token.rs
+++ djs_parser/src/token.rs
@@ -8,3 +8,26 @@ pub struct Token<'src> {
     pub text: &'src str,
     pub line: u32,
 }
+
+impl Token<'_> {
+    pub fn is_get(&self) -> bool {
+        self.kind == TokenKind::Ident && self.text == "get"
+    }
+    pub fn is_set(&self) -> bool {
+        self.kind == TokenKind::Ident && self.text == "set"
+    }
+
+    pub fn is_accessor_type(&self) -> bool {
+        self.is_get() || self.is_set()
+    }
+
+    pub fn is_valid_property_name(&self) -> bool {
+        self.kind == TokenKind::Ident || self.kind.is_keyword()
+    }
+
+    pub fn can_start_object_property_name(&self) -> bool {
+        self.is_valid_property_name()
+            || self.kind == TokenKind::LSquare
+            || self.kind == TokenKind::String
+    }
+}
