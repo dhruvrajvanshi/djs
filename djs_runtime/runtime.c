08823d070665665f70bcae625a2d41020b3ea404
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-03-11 12:41:19 +0100

    Move printing logic to a separate file

--- djs_runtime/runtime.c
+++ djs_runtime/runtime.c
@@ -1,46 +1,12 @@
-#include "runtime.h"
+#include "./runtime.h"
+#include "./print.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct DJSRuntime {
 } DJSRuntime;
-
-void DJSString_print(FILE *file, const DJSString *str) {
-  for (size_t i = 0; i < str->length; i++) {
-    fputc(str->value[i], file);
-  }
-}
-
-void DJSValue_print(FILE *file, const DJSValue *value) {
-  switch (value->type) {
-  case DJS_TYPE_UNDEFINED:
-    fprintf(file, "undefined");
-    break;
-  case DJS_TYPE_NULL:
-    fprintf(file, "null");
-    break;
-  case DJS_TYPE_BOOLEAN:
-    if (value->as.boolean) {
-      fprintf(file, "true");
-    } else {
-      fprintf(file, "false");
-    }
-    break;
-  case DJS_TYPE_NUMBER:
-    fprintf(file, "%f", value->as.number);
-    break;
-  case DJS_TYPE_OBJECT:
-    switch (value->as.object->type) {
-    case DJS_OBJECT_STRING:
-      DJSString_print(file, (DJSString *)(value->as.object));
-      break;
-    default:
-      fprintf(file, "[object: Object]");
-    }
-    break;
-  }
-}
 
 DJSRuntime *djs_new_runtime() {
   DJSRuntime *runtime = malloc(sizeof(DJSRuntime));
