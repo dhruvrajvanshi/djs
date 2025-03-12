eaead86b3381af8b25075d1156026aae1fe89722
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-03-12 12:29:20 +0100

    Move common stuff to the prelude

---
+++ djs_runtime/prelude.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifdef __GNUC__
+#define UNUSED(x) UNUSED_##x __attribute__((__unused__))
+#else
+#define UNUSED(x) UNUSED_##x
+#endif
+
+#ifdef __GNUC__
+#define UNUSED_FUNCTION(x) __attribute__((__unused__)) UNUSED_##x
+#else
+#define UNUSED_FUNCTION(x) UNUSED_##x
+#endif
+
+#ifdef __GNUC__
+#define auto __auto_type
+#else
+#error "auto is not supported"
+#endif
