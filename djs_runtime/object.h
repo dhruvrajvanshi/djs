cc608809dc55803d10182cbf53ed76a62f5f3abb
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-03-20 11:45:07 +0100

    Remove unused function

--- djs_runtime/object.h
+++ djs_runtime/object.h
@@ -18,10 +18,6 @@ typedef struct DJSObjectEntry {
   DJSObjectEntry *next;
 } DJSObjectEntry;
 
-static inline DJSValue DJSObject_as_value(DJSObject *object) {
-  return (DJSValue){.type = DJS_TYPE_OBJECT, .as = {.object = object}};
-}
-
 DJSObject *DJS_MakeBasicObject(DJSRuntime *runtime);
 
 /// https://tc39.es/ecma262/#sec-ordinarygetownproperty
