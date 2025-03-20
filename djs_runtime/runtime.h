91da1e738604cccb2f31fdce033ef010c39b25b6
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-03-20 11:48:10 +0100

    Make object abstract

--- djs_runtime/runtime.h
+++ djs_runtime/runtime.h
@@ -24,12 +24,6 @@ static inline DJSCompletion DJSCompletion_abrupt(DJSValue value) {
 typedef struct DJSRuntime DJSRuntime;
 DJSRuntime *djs_new_runtime(void);
 void djs_free_runtime(DJSRuntime *);
-DJSString *djs_new_string(DJSRuntime *, const char *value);
-
-DJSCompletion djs_object_set(DJSRuntime *, DJSObject *obj, DJSString *key,
-                             DJSValue value);
-
-DJSCompletion djs_object_get(DJSRuntime *, DJSObject *obj, DJSString *key);
 
 /// https://tc39.es/ecma262/#sec-isstrictlyequal
 bool DJS_IsStrictlyEqual(DJSValue left, DJSValue right);
--- djs_runtime/test_object_get.c
+++ djs_runtime/test_object_get.c
@@ -1,7 +1,3 @@
-#include "./print.h"
-#include "./runtime.h"
-#include "object.h"
-#include "value.h"
 #include <assert.h>
 
 #define ASSERT_EQEQEQ(left, right)                                             \
@@ -19,35 +15,4 @@
     exit(1);                                                                   \
   }
 
-int main(void) {
-  DJSRuntime *runtime = djs_new_runtime();
-  DJSObject *obj = DJS_MakeBasicObject(runtime);
-  DJSString *key = djs_new_string(runtime, "Key");
-
-  auto get_result = djs_object_get(runtime, obj, key);
-  assert(get_result.abrupt == false);
-  assert(get_result.value.type == DJS_TYPE_UNDEFINED);
-
-  DJSString *value = djs_new_string(runtime, "Value");
-
-  auto set_result =
-      djs_object_set(runtime, obj, key, DJSString_to_value(value));
-  assert(set_result.abrupt == false);
-
-  get_result = djs_object_get(runtime, obj, key);
-  assert(get_result.abrupt == false);
-
-  ASSERT_EQEQEQ(DJSString_to_value(value), get_result.value);
-
-  auto *key2 = djs_new_string(runtime, "Key2");
-  auto *value2 = djs_new_string(runtime, "Value2");
-
-  set_result = djs_object_set(runtime, obj, key2, DJSString_to_value(value2));
-  assert(set_result.abrupt == false);
-
-  get_result = djs_object_get(runtime, obj, key2);
-
-  ASSERT_EQEQEQ(DJSString_to_value(value2), get_result.value);
-
-  djs_free_runtime(runtime);
-}
+int main(void) {}
