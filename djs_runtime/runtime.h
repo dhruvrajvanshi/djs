b3484e20db6aa677cee9b552cbcd01570f4d77fa
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-03-20 11:55:39 +0100

    Move completion to a separate header

--- djs_runtime/runtime.h
+++ djs_runtime/runtime.h
@@ -8,19 +8,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct DJSCompletion {
-  DJSValue value;
-  bool abrupt;
-} DJSCompletion;
-
-static inline DJSCompletion DJSCompletion_normal(DJSValue value) {
-  return (DJSCompletion){.value = value, .abrupt = false};
-}
-
-static inline DJSCompletion DJSCompletion_abrupt(DJSValue value) {
-  return (DJSCompletion){.value = value, .abrupt = true};
-}
-
 typedef struct DJSRuntime DJSRuntime;
 DJSRuntime *djs_new_runtime(void);
 void djs_free_runtime(DJSRuntime *);
