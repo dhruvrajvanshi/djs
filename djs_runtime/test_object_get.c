d0e2fc316e6c53af8540ca389b196228c01affe4
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-03-23 11:46:04 +0100

    Factor out test prelude

--- djs_runtime/test_object_get.c
+++ djs_runtime/test_object_get.c
@@ -1,25 +1,9 @@
-#include <assert.h>
 #include "./completion.h"
 #include "./object.h"
-#include "./print.h"
-#include "./property.h"
 #include "./runtime.h"
 #include "./value.h"
-
-#define ASSERT_EQEQEQ(left, right)                                           \
-  if (!DJS_IsStrictlyEqual(left, right)) {                                   \
-    DJSValue left_value = left;                                              \
-    DJSValue right_value = right;                                            \
-    fprintf(stderr, "Assertion failed:%s:%d: in function %s:\n\t%s == %s\n", \
-            __FILE__, __LINE__, __FUNCTION__, #left, #right);                \
-    fprintf(stderr, "Because:\n");                                           \
-    fprintf(stderr, "\t");                                                   \
-    DJSValue_pretty_print(stderr, left_value);                               \
-    fprintf(stderr, " != ");                                                 \
-    DJSValue_pretty_print(stderr, right_value);                              \
-    fprintf(stderr, "\n");                                                   \
-    exit(1);                                                                 \
-  }
+#include "./test_prelude.h"
+#include <assert.h>
 
 int main(void) {
   DJSRuntime* runtime = djs_new_runtime();
