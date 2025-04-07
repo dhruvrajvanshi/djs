482a395a48187d923faefcd6db4accff9a7f933b
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-04-07 12:37:40 +0200

    Rename DJS_new_string -> DJS_new_string_as_value

--- djs_runtime/runtime.h
+++ djs_runtime/runtime.h
@@ -11,6 +11,7 @@ void djs_free_runtime(DJSRuntime*);
 /// https://tc39.es/ecma262/#sec-isstrictlyequal
 bool DJS_IsStrictlyEqual(DJSValue left, DJSValue right);
 
-DJSValue DJS_new_string(DJSRuntime*, const char* cstr);
+DJSValue DJS_new_string_as_value(DJSRuntime*, const char* cstr);
+DJSString* DJS_new_string(DJSRuntime*, const char* cstr);
 
 void djs_console_log(DJSRuntime*, DJSValue value);
