7c7d5a9e6d24203bff2e59b5eae7032d742a138b
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-04-17 19:26:54 +0200

    Add type definitions for IL nodes

--- djs_il/index.ts
+++ djs_il/index.ts
@@ -1,3 +1,32 @@
-export class DILFunction {}
-export class DISBasicBlock {}
-export class DILInstruction {}
+export type DILInstruction =
+  | { kind: 'make_basic_object'; name: DILRegister }
+  | { kind: 'set'; DILRegister: string; lhs: DILOperand; rhs: DILOperand }
+  | { kind: 'get'; DILRegister: string; lhs: DILOperand; rhs: DILOperand }
+
+export type DILRegister = string
+
+export type DILOperand =
+  | { kind: 'local'; name: string }
+  | { kind: 'constant'; value: DILConstant }
+
+export type DILConstant =
+  | {
+      kind: 'string'
+      value: string
+    }
+  | { kind: 'number'; value: number }
+  | { kind: 'boolean'; value: boolean }
+
+export type DILBasicBlock = {
+  params: DILParam[]
+  instructions: DILInstruction[]
+}
+
+export type DILType = { kind: 'top' }
+export type DILParam = { name: string; type: DILType }
+
+export type DILFunction = {
+  name: string
+  params: DILParam[]
+  blocks: DILBasicBlock[]
+}
