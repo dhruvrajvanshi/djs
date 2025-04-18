e369144f894e83776dba0322c190f468a55606e8
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-04-18 12:41:12 +0200

    Add jump if instruction

--- djs_il/dil.test.ts
+++ djs_il/dil.test.ts
@@ -11,10 +11,16 @@ test('builder and pretty printer', async () => {
         b.emit_make_object('%obj')
         b.emit_set(b.local('%obj'), b.string('key'), b.string('value'))
         b.emit_get('%value', b.local('%obj'), b.string('key'))
-        b.emit_call('%capitalized', b.global('@builtin.capitalize'), [
-          b.local('%value'),
-        ])
-        b.emit_return(b.local('%capitalized'))
+        b.emit_jump_if(b.local('%value'), '.if_true', '.if_false')
+        b.add_block('.if_true', () => {
+          b.emit_call('%capitalized', b.global('@builtin.capitalize'), [
+            b.local('%value'),
+          ])
+          b.emit_return(b.local('%capitalized'))
+        })
+        b.add_block('.if_false', () => {
+          b.emit_return(b.string('default'))
+        })
       }),
     ),
   ).toMatchFileSnapshot('test_snapshots/build_function.dil')
