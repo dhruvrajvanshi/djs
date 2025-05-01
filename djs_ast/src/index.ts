9dcdbeebc37e146ecfe63b3c139b3faac8785af8
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-05-01 18:14:06 +0200

    Show errors on the console

--- djs_ast/src/index.ts
+++ djs_ast/src/index.ts
@@ -1,4 +1,5 @@
 import { pretty_print } from "./pretty_print.js"
+import { Span } from "./Span.js"
 
 export { TokenKind } from "./TokenKind.js"
 export type { Token } from "./Token.js"
@@ -13,7 +14,8 @@ export {
 async function main(path: string) {
   const fs = await import("node:fs/promises")
   const { Parser } = await import("./parser.js")
-  const parser = Parser(await fs.readFile(path, "utf-8"))
+  const source_text = await fs.readFile(path, "utf-8")
+  const parser = Parser(source_text)
   const source_file = parser.parse_source_file()
   console.log(
     pretty_print(source_file)
@@ -21,11 +23,50 @@ async function main(path: string) {
       .map((line, idx) => `${idx + 1}: ${line}`)
       .join("\n"),
   )
+  const start_red_and_bold = "\x1b[31;1m"
+  const reset_color = "\x1b[0m"
   console.log(
     source_file.errors
-      .map((e) => `${path}:${e.span.start}: ERROR: ${e.message}`)
-      .join("\n"),
+      .map(
+        (e) =>
+          `${start_red_and_bold}ERROR:${reset_color} ${path}:${offset_to_line(source_text, e.span.start)}: ${e.message}\n${preview_lines(source_text, e.span)}`,
+      )
+      .join("\n\n"),
   )
+}
+function preview_lines(source: string, span: Span) {
+  const [start_line, col] = offset_to_line_and_col(source, span.start)
+  const lines = source.split("\n").slice(start_line - 1, start_line + 2)
+  return lines
+    .map((line, idx) => {
+      const line_number = start_line + idx
+      const prefix = `${line_number}|  `
+      const first = `${prefix}${line}`
+      if (idx === 0) {
+        return first + "\n" + "~".repeat(prefix.length + col - 1) + "^"
+      } else return first
+    })
+    .join("\n")
+}
+function offset_to_line(source: string, offset: number): number {
+  return offset_to_line_and_col(source, offset)[0]
+}
+function offset_to_line_and_col(
+  source: string,
+  offset: number,
+): [number, number] {
+  const lines = source.split("\n")
+  let line_number = 1
+  let col_number = offset + 1
+  for (const line of lines) {
+    if (offset < line.length) {
+      return [line_number, col_number]
+    }
+    offset -= line.length + 1
+    line_number++
+    col_number = offset + 1
+  }
+  return [line_number, col_number]
 }
 if (process.argv[1] === import.meta.filename) {
   await main(process.argv[2])
