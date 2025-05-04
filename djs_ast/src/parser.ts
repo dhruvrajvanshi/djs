2b6df44d9b16932e34eb9ce8ba34ff58695d6e15
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-05-04 10:54:21 +0200

    Create diagnostic.ts

--- djs_ast/src/parser.ts
+++ djs_ast/src/parser.ts
@@ -49,16 +49,9 @@ function parser_impl(source: string, _lexer: Lexer): Parser {
       errors: [...errors],
     }
     is_forked = true
-    preview_lines(source, snapshot.current_token.span)
-    console.trace(
-      "Forking parser state",
-      current_token.kind,
-      offset_to_line_and_col(source, snapshot.current_token.span.start),
-    )
 
     return function restore(value) {
       is_forked = false
-      console.log("Restoring parser state")
       lexer = snapshot.lexer
       last_token = snapshot.last_token
       current_token = snapshot.current_token
@@ -1183,47 +1176,4 @@ function assign_op(kind: TokenKind): AssignOp | null {
     default:
       return null
   }
-}
-
-const COLOR_ERROR = "\x1b[31;1m"
-const COLOR_RESET = "\x1b[0m"
-const COLOR_DIMMED = "\x1b[2m"
-function preview_lines(source: string, span: Span) {
-  const [start_line, col] = offset_to_line_and_col(source, span.start)
-  const lines = source.split("\n").slice(start_line - 1, start_line + 2)
-  return lines
-    .map((line, idx) => {
-      const line_number = start_line + idx
-      const prefix = `${line_number}|  `
-      const first = `${COLOR_DIMMED}${prefix}${COLOR_RESET}${line}`
-      if (idx === 0) {
-        return (
-          first +
-          "\n" +
-          " ".repeat(prefix.length + col - 1) +
-          `${COLOR_ERROR}^~~${COLOR_RESET}`
-        )
-      } else return first
-    })
-    .join("\n")
-}
-function offset_to_line(source: string, offset: number): number {
-  return offset_to_line_and_col(source, offset)[0]
-}
-function offset_to_line_and_col(
-  source: string,
-  offset: number,
-): [number, number] {
-  const lines = source.split("\n")
-  let line_number = 1
-  let col_number = offset + 1
-  for (const line of lines) {
-    if (offset < line.length) {
-      return [line_number, col_number]
-    }
-    offset -= line.length + 1
-    line_number++
-    col_number = offset + 1
-  }
-  return [line_number, col_number]
 }
