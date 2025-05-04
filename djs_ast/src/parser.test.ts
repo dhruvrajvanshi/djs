bc5ead9bf9b629a2fb4f26c0fb8ae63a57ab6fdd
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-05-04 18:44:21 +0200

    Parser test

--- djs_ast/src/parser.test.ts
+++ djs_ast/src/parser.test.ts
@@ -3,6 +3,7 @@ import { Parser } from "./parser"
 import { Expr } from "./ast.gen"
 import { expect } from "vitest"
 import { pretty_print } from "./pretty_print"
+import fs from "fs/promises"
 
 test("test_parse_var_expr", () => {
   const expr = parse_expr("x")
@@ -33,6 +34,56 @@ test("parse error for missing parenthesis in if condition", () => {
     7: Missing semicolon"
   `)
 })
+
+const test262Paths: string[] = []
+
+for await (const path of fs.glob("../test262/test/**/*.js")) {
+  test262Paths.push(path)
+}
+test.each(test262Paths)("test262: %s", async (filePath) => {
+  // Skip staging files
+  if (filePath.includes("staging")) {
+    return
+  }
+
+  // Skip known problematic files that cause stack overflow
+  if (filePath.includes("test/language/statements/function/S13.2.1_A1_T1.js")) {
+    return
+  }
+
+  const source = await fs.readFile(filePath, "utf-8")
+
+  const expectedParseError = syntax_error_expected(source)
+  const parser = Parser(source)
+  const source_file = parser.parse_source_file()
+  if (expectedParseError) {
+    // If the test262 file is expected to throw a syntax error, check if it does
+    const errors = source_file.errors
+    expect(errors.length).toBeGreaterThan(0)
+  } else {
+    // If the test262 file is not expected to throw a syntax error, check if it does not
+    expect(source_file.errors).toEqual([])
+  }
+})
+
+function syntax_error_expected(source: string): boolean {
+  const frontmatter = extract_frontmatter(source)
+  const isNegative = frontmatter.includes("negative:")
+  const syntaxErrorExpected =
+    frontmatter.includes("phase: parse") &&
+    frontmatter.includes("type: SyntaxError")
+  return isNegative && syntaxErrorExpected
+}
+
+function extract_frontmatter(source: string): string {
+  const frontmatterStart = source.indexOf("/*---")
+  if (frontmatterStart === -1) return ""
+
+  const frontmatterEnd = source.indexOf("---*/")
+  if (frontmatterEnd === -1) return ""
+
+  return source.substring(frontmatterStart, frontmatterEnd)
+}
 
 function parse_expr(input: string): Expr {
   const parser = Parser(input)
