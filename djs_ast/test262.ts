9a610cfc1643949b7ddf11c0f82e7f825d9657b4
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-05-06 11:29:12 +0200

    test262

--- djs_ast/test262.ts
+++ djs_ast/test262.ts
@@ -1,5 +1,6 @@
 import fs from "node:fs/promises"
 import { Parser } from "./src/parser"
+import assert from "node:assert"
 
 const test262Paths: string[] = []
 
@@ -15,8 +16,54 @@ for await (const path of fs.glob("../test262/test/**/*.js")) {
   test262Paths.push(path)
 }
 
+const successes: string[] = []
+const failures: string[] = []
 for (const path of test262Paths) {
   const source = await fs.readFile(path, "utf-8")
   console.log(`Parsing ${path}...`)
   const errors = Parser(source).parse_source_file().errors
+  if (syntax_error_expected(source)) {
+    if (errors.length === 0) {
+      failures.push(path)
+    } else {
+      successes.push(path)
+    }
+  } else {
+    if (errors.length > 0) {
+      failures.push(path)
+    } else {
+      successes.push(path)
+    }
+  }
+}
+
+const expected_parse_failures = (
+  await fs.readFile("../djs_parser/test_262_baseline.failed.txt", "utf-8")
+).split("\n")
+const expected_parse_successes = (
+  await fs.readFile("../djs_parser/test_262_baseline.success.txt", "utf-8")
+).split("\n")
+console.log(`Successes: ${successes.length}`)
+console.log(`Failures: ${successes.length}`)
+
+assert.equal(successes.length, expected_parse_successes.length)
+assert.equal(failures.length, expected_parse_failures.length)
+
+function syntax_error_expected(source: string): boolean {
+  const frontmatter = extract_frontmatter(source)
+  const isNegative = frontmatter.includes("negative:")
+  const syntaxErrorExpected =
+    frontmatter.includes("phase: parse") &&
+    frontmatter.includes("type: SyntaxError")
+  return isNegative && syntaxErrorExpected
+}
+
+function extract_frontmatter(source: string): string {
+  const frontmatterStart = source.indexOf("/*---")
+  if (frontmatterStart === -1) return ""
+
+  const frontmatterEnd = source.indexOf("---*/")
+  if (frontmatterEnd === -1) return ""
+
+  return source.substring(frontmatterStart, frontmatterEnd)
 }
