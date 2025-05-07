d137d72e30c94b5d4ea5ea96e6ec5a42ada52953
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-05-07 11:36:13 +0200

    Lex numbers

--- djs_frontend/src/lexer.ts
+++ djs_frontend/src/lexer.ts
@@ -107,9 +107,7 @@ export function lexer_impl(
     }
 
     // '{' => self.lex_single_char_token(TokenKind::LBrace),
-    else if (at("{")) {
-      return lex_single_char_token(TokenKind.LBrace)
-    }
+    else if (at("{")) return lex_single_char_token(TokenKind.LBrace)
     // '}' if self.template_literal_interpolation => {
     //     self.end_template_literal_interpolation();
     //     self.lex_template_literal_post_interpolation()
@@ -119,235 +117,129 @@ export function lexer_impl(
       return lex_template_literal_post_interpolation()
     }
     // '}' => self.lex_single_char_token(TokenKind::RBrace),
-    else if (at("}")) {
-      return lex_single_char_token(TokenKind.RBrace)
-    }
+    else if (at("}")) return lex_single_char_token(TokenKind.RBrace)
     // '(' => self.lex_single_char_token(TokenKind::LParen),
-    else if (at("(")) {
-      return lex_single_char_token(TokenKind.LParen)
-    }
+    else if (at("(")) return lex_single_char_token(TokenKind.LParen)
     // ')' => self.lex_single_char_token(TokenKind::RParen),
-    else if (at(")")) {
-      return lex_single_char_token(TokenKind.RParen)
-    }
+    else if (at(")")) return lex_single_char_token(TokenKind.RParen)
     // ';' => self.lex_single_char_token(TokenKind::Semi),
-    else if (at(";")) {
-      return lex_single_char_token(TokenKind.Semi)
-    }
+    else if (at(";")) return lex_single_char_token(TokenKind.Semi)
     // '[' => self.lex_single_char_token(TokenKind::LSquare),
-    else if (at("[")) {
-      return lex_single_char_token(TokenKind.LSquare)
-    }
+    else if (at("[")) return lex_single_char_token(TokenKind.LSquare)
     // ']' => self.lex_single_char_token(TokenKind::RSquare),
-    else if (at("]")) {
-      return lex_single_char_token(TokenKind.RSquare)
-    }
+    else if (at("]")) return lex_single_char_token(TokenKind.RSquare)
     // ',' => self.lex_single_char_token(TokenKind::Comma),
-    else if (at(",")) {
-      return lex_single_char_token(TokenKind.Comma)
-    }
+    else if (at(",")) return lex_single_char_token(TokenKind.Comma)
     // ':' => self.lex_single_char_token(TokenKind::Colon),
-    else if (at(":")) {
-      return lex_single_char_token(TokenKind.Colon)
-    }
+    else if (at(":")) return lex_single_char_token(TokenKind.Colon)
     // _ if at!("...") => self.lex_3_char_token(TokenKind::DotDotDot),
-    else if (at("...")) {
-      return lex_3_char_token(TokenKind.DotDotDot)
-    }
+    else if (at("...")) return lex_3_char_token(TokenKind.DotDotDot)
     // '.' => self.lex_single_char_token(TokenKind::Dot),
-    else if (at(".")) {
-      return lex_single_char_token(TokenKind.Dot)
-    }
-
+    else if (at(".")) return lex_single_char_token(TokenKind.Dot)
     // _ if at!("&&=") => self.lex_3_char_token(TokenKind::AmpAmpEq),
-    else if (at("&&=")) {
-      return lex_3_char_token(TokenKind.AmpAmpEq)
-    }
+    else if (at("&&=")) return lex_3_char_token(TokenKind.AmpAmpEq)
     // _ if at!("||=") => self.lex_3_char_token(TokenKind::BarBarEq),
-    else if (at("||=")) {
-      return lex_3_char_token(TokenKind.BarBarEq)
-    }
+    else if (at("||=")) return lex_3_char_token(TokenKind.BarBarEq)
     // _ if at!("??=") => self.lex_3_char_token(TokenKind::QuestionQuestionEq),
-    else if (at("??=")) {
-      return lex_3_char_token(TokenKind.QuestionQuestionEq)
-    }
+    else if (at("??=")) return lex_3_char_token(TokenKind.QuestionQuestionEq)
     // _ if at!("*=") => self.lex_2_char_token(TokenKind::StarEq),
-    else if (at("*=")) {
-      return lex_2_char_token(TokenKind.StarEq)
-    }
+    else if (at("*=")) return lex_2_char_token(TokenKind.StarEq)
     // _ if at!("/=") => self.lex_2_char_token(TokenKind::SlashEq),
-    else if (at("/=")) {
-      return lex_2_char_token(TokenKind.SlashEq)
-    }
+    else if (at("/=")) return lex_2_char_token(TokenKind.SlashEq)
     // _ if at!("%=") => self.lex_2_char_token(TokenKind::PercentEq),
-    else if (at("%=")) {
-      return lex_2_char_token(TokenKind.PercentEq)
-    }
+    else if (at("%=")) return lex_2_char_token(TokenKind.PercentEq)
     // _ if at!("+=") => self.lex_2_char_token(TokenKind::PlusEq),
-    else if (at("+=")) {
-      return lex_2_char_token(TokenKind.PlusEq)
-    }
+    else if (at("+=")) return lex_2_char_token(TokenKind.PlusEq)
     // _ if at!("-=") => self.lex_2_char_token(TokenKind::MinusEq),
-    else if (at("-=")) {
-      return lex_2_char_token(TokenKind.MinusEq)
-    }
+    else if (at("-=")) return lex_2_char_token(TokenKind.MinusEq)
     // _ if at!("<<=") => self.lex_3_char_token(TokenKind::LessThanLessThanEq),
-    else if (at("<<=")) {
-      return lex_3_char_token(TokenKind.LessThanLessThanEq)
-    }
+    else if (at("<<=")) return lex_3_char_token(TokenKind.LessThanLessThanEq)
     // _ if at!("<<") => self.lex_3_char_token(TokenKind::LessThanLessThan),
-    else if (at("<<")) {
-      return lex_2_char_token(TokenKind.LessThanLessThan)
-    }
+    else if (at("<<")) return lex_2_char_token(TokenKind.LessThanLessThan)
     // _ if at!(">>=") => self.lex_3_char_token(TokenKind::GreaterThanGreaterThanEq),
-    else if (at(">>=")) {
+    else if (at(">>="))
       return lex_3_char_token(TokenKind.GreaterThanGreaterThanEq)
-    }
     // _ if at!(">>>=") => {
     //     self.lex_4_char_token(TokenKind::GreaterThanGreaterThanGreaterThanEq)
     // }
-    else if (at(">>>=")) {
+    else if (at(">>>="))
       return lex_n_char_token(4, TokenKind.GreaterThanGreaterThanGreaterThanEq)
-    }
     // _ if at!(">>>") => self.lex_3_char_token(TokenKind::GreaterThanGreaterThanGreaterThan),
-    else if (at(">>>")) {
+    else if (at(">>>"))
       return lex_3_char_token(TokenKind.GreaterThanGreaterThanGreaterThan)
-    }
     // _ if at!("&=") => self.lex_2_char_token(TokenKind::AmpEq),
-    else if (at("&=")) {
-      return lex_2_char_token(TokenKind.AmpEq)
-    }
+    else if (at("&=")) return lex_2_char_token(TokenKind.AmpEq)
     // _ if at!("^=") => self.lex_2_char_token(TokenKind::CaretEq),
-    else if (at("^=")) {
-      return lex_2_char_token(TokenKind.CaretEq)
-    }
+    else if (at("^=")) return lex_2_char_token(TokenKind.CaretEq)
     // _ if at!("|=") => self.lex_2_char_token(TokenKind::BarEq),
-    else if (at("|=")) {
-      return lex_2_char_token(TokenKind.BarEq)
-    }
+    else if (at("|=")) return lex_2_char_token(TokenKind.BarEq)
     // _ if at!("**=") => self.lex_3_char_token(TokenKind::StarStarEq),
-    else if (at("**=")) {
-      return lex_3_char_token(TokenKind.StarStarEq)
-    }
+    else if (at("**=")) return lex_3_char_token(TokenKind.StarStarEq)
     // '%' => self.lex_single_char_token(TokenKind::Percent),
-    else if (at("%")) {
-      return lex_single_char_token(TokenKind.Percent)
-    }
+    else if (at("%")) return lex_single_char_token(TokenKind.Percent)
     // '*' => self.lex_single_char_token(TokenKind::Star),
-    else if (at("*")) {
-      return lex_single_char_token(TokenKind.Star)
-    }
+    else if (at("*")) return lex_single_char_token(TokenKind.Star)
     // '~' => self.lex_single_char_token(TokenKind::Tilde),
-    else if (at("~")) {
-      return lex_single_char_token(TokenKind.Tilde)
-    }
+    else if (at("~")) return lex_single_char_token(TokenKind.Tilde)
     // '/' if self.regex_enabled => self.lex_regex(),
     else if (regex_enabled && at("/")) return lex_regex()
     // '/' => self.lex_single_char_token(TokenKind::Slash),
     else if (at("/")) return lex_single_char_token(TokenKind.Slash)
     // '?' => self.lex_single_char_token(TokenKind::Question),
-    else if (at("?")) {
-      return lex_single_char_token(TokenKind.Question)
-    }
+    else if (at("?")) return lex_single_char_token(TokenKind.Question)
     // '"' | '\'' => self.lex_simple_string(),
-    else if (at('"') || at("'")) {
-      return lex_simple_string()
-    }
+    else if (at('"') || at("'")) return lex_simple_string()
     // '`' => self.lex_template_literal_start(),
-    else if (at("`")) {
-      return lex_template_literal_start()
-    }
-
+    else if (at("`")) return lex_template_literal_start()
     // _ if at!("<=") => self.lex_2_char_token(TokenKind::LessThanEq),
-    else if (at("<=")) {
-      return lex_2_char_token(TokenKind.LessThanEq)
-    }
+    else if (at("<=")) return lex_2_char_token(TokenKind.LessThanEq)
     // '<' => self.lex_single_char_token(TokenKind::LessThan),
-    else if (at("<")) {
-      return lex_single_char_token(TokenKind.LessThan)
-    }
-
+    else if (at("<")) return lex_single_char_token(TokenKind.LessThan)
     // _ if at!(">=") => self.lex_2_char_token(TokenKind::GreaterThanEq),
-    else if (at(">=")) {
-      return lex_2_char_token(TokenKind.GreaterThanEq)
-    }
+    else if (at(">=")) return lex_2_char_token(TokenKind.GreaterThanEq)
     // '>' => self.lex_single_char_token(TokenKind::GreaterThan),
-    else if (at(">")) {
-      return lex_single_char_token(TokenKind.GreaterThan)
-    }
-
+    else if (at(">")) return lex_single_char_token(TokenKind.GreaterThan)
     // _ if at!("!==") => self.lex_3_char_token(TokenKind::BangEqEq),
-    else if (at("!==")) {
-      return lex_3_char_token(TokenKind.BangEqEq)
-    }
+    else if (at("!==")) return lex_3_char_token(TokenKind.BangEqEq)
     // _ if at!("!=") => self.lex_2_char_token(TokenKind::BangEq),
-    else if (at("!=")) {
-      return lex_2_char_token(TokenKind.BangEq)
-    }
+    else if (at("!=")) return lex_2_char_token(TokenKind.BangEq)
     // '!' => self.lex_single_char_token(TokenKind::Bang),
-    else if (at("!")) {
-      return lex_single_char_token(TokenKind.Bang)
-    }
-
+    else if (at("!")) return lex_single_char_token(TokenKind.Bang)
     // _ if at!("===") => self.lex_3_char_token(TokenKind::EqEqEq),
-    else if (at("===")) {
-      return lex_3_char_token(TokenKind.EqEqEq)
-    }
+    else if (at("===")) return lex_3_char_token(TokenKind.EqEqEq)
     // _ if at!("==") => self.lex_2_char_token(TokenKind::EqEq),
-    else if (at("==")) {
-      return lex_2_char_token(TokenKind.EqEq)
-    }
+    else if (at("==")) return lex_2_char_token(TokenKind.EqEq)
     // _ if at!("=>") => self.lex_2_char_token(TokenKind::FatArrow),
-    else if (at("=>")) {
-      return lex_2_char_token(TokenKind.FatArrow)
-    }
+    else if (at("=>")) return lex_2_char_token(TokenKind.FatArrow)
     // '=' => self.lex_single_char_token(TokenKind::Eq),
-    else if (at("=")) {
-      return lex_single_char_token(TokenKind.Eq)
-    }
-
+    else if (at("=")) return lex_single_char_token(TokenKind.Eq)
     // _ if at!("++") => self.lex_2_char_token(TokenKind::PlusPlus),
-    else if (at("++")) {
-      return lex_2_char_token(TokenKind.PlusPlus)
-    }
+    else if (at("++")) return lex_2_char_token(TokenKind.PlusPlus)
     // '+' => self.lex_single_char_token(TokenKind::Plus),
-    else if (at("+")) {
-      return lex_single_char_token(TokenKind.Plus)
-    }
-
+    else if (at("+")) return lex_single_char_token(TokenKind.Plus)
     // _ if at!("--") => self.lex_2_char_token(TokenKind::MinusMinus),
-    else if (at("--")) {
-      return lex_2_char_token(TokenKind.MinusMinus)
-    }
+    else if (at("--")) return lex_2_char_token(TokenKind.MinusMinus)
     // '-' => self.lex_single_char_token(TokenKind::Minus),
-    else if (at("-")) {
-      return lex_single_char_token(TokenKind.Minus)
-    }
-
+    else if (at("-")) return lex_single_char_token(TokenKind.Minus)
     // _ if at!("&&") => self.lex_2_char_token(TokenKind::AmpAmp),
-    else if (at("&&")) {
-      return lex_2_char_token(TokenKind.AmpAmp)
-    }
+    else if (at("&&")) return lex_2_char_token(TokenKind.AmpAmp)
     // '&' => self.lex_single_char_token(TokenKind::Amp),
-    else if (at("&")) {
-      return lex_single_char_token(TokenKind.Amp)
-    }
-
+    else if (at("&")) return lex_single_char_token(TokenKind.Amp)
     // _ if at!("||") => self.lex_2_char_token(TokenKind::VBarVBar),
-    else if (at("||")) {
-      return lex_2_char_token(TokenKind.VBarVBar)
-    }
+    else if (at("||")) return lex_2_char_token(TokenKind.VBarVBar)
     // '|' => self.lex_single_char_token(TokenKind::VBar),
-    else if (at("|")) {
-      return lex_single_char_token(TokenKind.VBar)
-    }
-
+    else if (at("|")) return lex_single_char_token(TokenKind.VBar)
     // '^' => self.lex_single_char_token(TokenKind::Caret),
-    else if (at("^")) {
-      return lex_single_char_token(TokenKind.Caret)
-    } else if (is_ascii_digit(current_char())) {
-      return lex_number()
-    } else {
+    else if (at("^")) return lex_single_char_token(TokenKind.Caret)
+    // _ if at!("0x") || at!("0X") => self.lex_hex_number(),
+    else if (at("0x") || at("0X")) return lex_hex_number()
+    // _ if at!("0o") || at!("0O") => self.lex_octal_number(),
+    else if (at("0o") || at("0O")) return lex_octal_number()
+    // _ if at!("0b") || at!("0b") => self.lex_binary_number(),
+    else if (at("0b") || at("0B")) return lex_binary_number()
+    // c if c.is_ascii_digit() => self.lex_number(),
+    else if (is_ascii_digit(current_char())) return lex_number()
+    else {
       let limit = 100
       while (current_char() !== "\0" && !is_whitespace(current_char())) {
         limit--
@@ -410,10 +302,154 @@ export function lexer_impl(
       }
     }
   }
+
   function lex_number(): Token {
-    while (is_ascii_digit(current_char())) {
+    const start = advance()
+    assert(is_ascii_digit(start), "Expected a digit at the start of a number")
+
+    // Parse integer part (and potentially decimal point + fraction part)
+    while (
+      (is_ascii_digit(current_char()) || current_char() === ".") &&
+      current_char() !== "\0"
+    ) {
       advance()
     }
+
+    // Handle exponent part (e.g., 1e10, 1e+10, 1e-10)
+    let has_exponent_part = false
+    if (current_char() === "e" || current_char() === "E") {
+      has_exponent_part = true
+      advance()
+
+      // Optional + or - after exponent indicator
+      if (current_char() === "+" || current_char() === "-") {
+        advance()
+      }
+
+      // Exponent must have at least one digit
+      if (!is_ascii_digit(current_char())) {
+        return error_token("Expected a digit after the exponent")
+      }
+
+      // Parse exponent digits
+      while (is_ascii_digit(current_char()) && current_char() !== "\0") {
+        advance()
+      }
+    }
+
+    // Handle BigInt suffix 'n'
+    if (current_char() === "n") {
+      advance()
+    }
+
+    const text = current_text()
+
+    // Validate: no leading zeros
+    const first_char = text.charAt(0)
+    const second_char = text.charAt(1)
+    if (first_char === "0" && second_char >= "0" && second_char <= "9") {
+      return error_token(
+        "Invalid number literal: leading 0s are not allowed; If you want to use an octal literal, use a 0o prefix.",
+      )
+    }
+
+    // Validate: BigInt cannot have decimal points
+    if (text.endsWith("n") && text.includes(".")) {
+      return error_token(
+        "Invalid number literal: decimal points are not allowed in BigInt literals",
+      )
+    }
+
+    // Validate: BigInt cannot have exponent part
+    if (text.endsWith("n") && has_exponent_part) {
+      return error_token(
+        "Invalid number literal: BigInt literals cannot have an exponent part",
+      )
+    }
+
+    return make_token(TokenKind.Number)
+  }
+  function lex_hex_number(): Token {
+    assert(current_char() === "0", "Expected '0' at the start of a hex number")
+    assert(
+      next_char() === "x" || next_char() === "X",
+      "Expected 'x' or 'X' after '0'",
+    )
+
+    advance() // Consume '0'
+    advance() // Consume 'x' or 'X'
+
+    if (!is_hex_digit(current_char())) {
+      return error_token("Expected a hex character")
+    }
+
+    while (is_hex_digit(current_char()) && current_char() !== "\0") {
+      advance()
+    }
+
+    // Handle optional BigInt suffix
+    if (current_char() === "n") {
+      advance()
+    }
+
+    return make_token(TokenKind.Number)
+  }
+
+  function lex_octal_number(): Token {
+    assert(
+      current_char() === "0",
+      "Expected '0' at the start of an octal number",
+    )
+    assert(
+      next_char() === "o" || next_char() === "O",
+      "Expected 'o' or 'O' after '0'",
+    )
+
+    advance() // Consume '0'
+    advance() // Consume 'o' or 'O'
+
+    if (!is_octal_digit(current_char())) {
+      return error_token("Expected an octal digit (0-7)")
+    }
+
+    while (is_octal_digit(current_char()) && current_char() !== "\0") {
+      advance()
+    }
+
+    // Handle optional BigInt suffix
+    if (current_char() === "n") {
+      advance()
+    }
+
+    return make_token(TokenKind.Number)
+  }
+
+  function lex_binary_number(): Token {
+    assert(
+      current_char() === "0",
+      "Expected '0' at the start of a binary number",
+    )
+    assert(
+      next_char() === "b" || next_char() === "B",
+      "Expected 'b' or 'B' after '0'",
+    )
+
+    advance() // Consume '0'
+    advance() // Consume 'b' or 'B'
+
+    if (!is_binary_digit(current_char())) {
+      return error_token("Expected 0 or 1")
+    }
+
+    while (is_binary_digit(current_char()) && current_char() !== "\0") {
+      advance()
+    }
+
+    // Handle optional BigInt suffix
+    if (current_char() === "n") {
+      advance()
+    }
+
     return make_token(TokenKind.Number)
   }
 
@@ -713,13 +749,19 @@ function is_hex_digit(c: string): boolean {
   )
 }
 
-// Helper function for regex parsing
 function is_line_terminator(c: string): boolean {
   return c === "\n" || c === "\r"
 }
 
-// Helper function for regex flags
 function is_valid_regex_flag(c: string): boolean {
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions#advanced_searching_with_flags
   return ["d", "g", "i", "m", "s", "u", "v", "y"].includes(c)
+}
+
+function is_octal_digit(c: string): boolean {
+  return c >= "0" && c <= "7"
+}
+
+function is_binary_digit(c: string): boolean {
+  return c === "0" || c === "1"
 }
