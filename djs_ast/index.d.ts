30375089d5b32da092161e26d352959d83975974
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-05-12 15:05:58 +0200

    Remove rust code

--- djs_ast/index.d.ts
+++
@@ -1,8 +0,0 @@
-
-type Type1 = ["Vec" | "Box" | "Option", Type];
-type Type = string | Type1;
-type Tag = "span" | "clone" | "visit";
-type EnumVariant = { name: string, args: Type[], tags: Tag[] };
-type EnumItem = { kind: "enum", name: string, tags: Tag[], variants: EnumVariant[] }
-type StructItem = { kind: "struct", name: string, tags: Tag[], fields: [string, Type][] }
-type Item = EnumItem | StructItem;
