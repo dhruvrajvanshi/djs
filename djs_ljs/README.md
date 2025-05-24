26633b443320ac22c0fe70c9e541f14648f98a44
Dhruv Rajvanshi <d@rajv.dev>
2025-05-24 16:45:28 +0200

    LJS: Parsing for pointer types and tagged template literals (#9)

---
+++ djs_ljs/README.md
@@ -0,0 +1,6 @@
+# LJS
+
+Low level sister language for DJS.
+
+In effect, it's C with JS syntax and some QOL features.
+Eventually, it will have opt in higher level features and DJS will be a superset of LJS.
--- djs_parser/run_parser.ts
+++ djs_parser/run_parser.ts
@@ -1,4 +1,7 @@
-import { show_diagnostics } from "./diagnostic.js"
+import { show_diagnostics } from "./diagnostic.ts"
+import fs from "node:fs/promises"
+import { Parser } from "./parser.ts"
+import { parseArgs } from "node:util"
 
 export {
   type Token,
@@ -11,20 +14,40 @@ export {
 } from "djs_ast"
 
 if (process.argv[1] === import.meta.filename) {
-  await main(process.argv.slice(2))
+  const { values, positionals } = parseArgs({
+    args: process.argv.slice(2),
+    allowPositionals: true,
+    options: {
+      "internal-throw-on-error": { type: "boolean", default: false },
+      raw: { type: "string" },
+    },
+  })
+  await main(positionals, {
+    throwOnError: values["internal-throw-on-error"],
+    raw: values.raw,
+  })
 }
 
-async function main(paths: string[]) {
-  const fs = await import("node:fs/promises")
-  const { Parser } = await import("./parser.js")
+interface Config {
+  throwOnError: boolean
+}
+async function main(paths: string[], config: Config & { raw?: string }) {
   let total_errors = 0
-  for (const path of paths) {
-    const source_text = await fs.readFile(path, "utf-8")
-    const parser = Parser(path, source_text)
+  if (config.raw) {
+    const parser = Parser("<raw input>", config.raw, config)
     const source_file = parser.parse_source_file()
-
-    show_diagnostics(path, source_text, source_file.errors)
+    console.dir(source_file, { depth: Infinity })
+    show_diagnostics("<raw input>", config.raw, source_file.errors)
     total_errors += source_file.errors.length
+  } else {
+    for (const path of paths) {
+      const source_text = await fs.readFile(path, "utf-8")
+      const parser = Parser(path, source_text, config)
+      const source_file = parser.parse_source_file()
+
+      show_diagnostics(path, source_text, source_file.errors)
+      total_errors += source_file.errors.length
+    }
   }
 
   console.log(`Found ${total_errors} error(s)`)
