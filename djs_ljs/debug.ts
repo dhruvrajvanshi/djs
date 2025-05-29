085b9b839f038d3b6b2cb6c09aba356601eb0933
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-05-29 17:04:04 +0200

    [ljs]: Follow imports to collect all entrypoints

--- djs_ljs/debug.ts
+++ djs_ljs/debug.ts
@@ -4,6 +4,8 @@ import { parseArgs } from "node:util"
 import fs from "node:fs/promises"
 import { parse_source_file, show_diagnostics } from "djs_parser"
 import { source_file_to_sexpr } from "../djs_ast/sexpr.ts"
+import type { SourceFile } from "djs_ast"
+import assert from "node:assert"
 
 const { positionals: files, values: args } = parseArgs({
   allowPositionals: true,
@@ -22,16 +24,50 @@ if (files.length > 1) {
   process.exit(1)
 }
 
-const entry_path = files[0]
+const source_files = await collect_source_files(files[0])
+console.log(source_files)
 
-const entry_source_text = await fs.readFile(entry_path, "utf-8")
-const entry_source_file = parse_source_file(entry_path, entry_source_text)
-if (entry_source_file.errors.length > 0 && !args["no-errors"]) {
-  console.log(
-    show_diagnostics(entry_path, entry_source_text, entry_source_file.errors),
-  )
+async function collect_source_files(entry_path: string) {
+  const queue: Queue<string> = [entry_path]
+  const source_files: Record<string, SourceFile> = {}
+  while (queue.length > 0) {
+    const path = queueTake(queue)
+    if (path === null) {
+      continue
+    }
+    const source_text = await fs.readFile(path, "utf-8")
+    const source_file = parse_source_file(path, source_text)
+
+    source_files[path] = source_file
+    const imports = collect_imports(source_file)
+    for (const import_path of imports) {
+      if (!(import_path in source_files)) {
+        queuePush(queue, import_path)
+      }
+    }
+  }
+  return source_files
 }
-if (args["dump-ast"]) {
-  console.log("AST Dump:")
-  console.dir(source_file_to_sexpr(entry_source_file), { depth: Infinity })
+function collect_imports(source_file: SourceFile): string[] {
+  const imports: string[] = []
+  for (const stmt of source_file.stmts) {
+    // TODO(visitor): Handle nested imports when the visitor API is implemented
+    if (stmt.kind === "Import") {
+      imports.push(parse_module_specifier(stmt.module_specifier))
+    }
+  }
+  return imports
+}
+function parse_module_specifier(specifier: string): string {
+  const value: unknown = JSON.parse(specifier)
+  assert(typeof value === "string", `Invalid module specifier: ${specifier}`)
+  return value
+}
+
+type Queue<T> = T[]
+function queueTake<T>(queue: Queue<T>): T | null {
+  return queue.shift() ?? null
+}
+function queuePush<T>(queue: Queue<T>, item: T): void {
+  queue.push(item)
 }
