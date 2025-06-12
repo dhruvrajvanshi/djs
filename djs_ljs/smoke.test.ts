0ef7b165b85ed733547d218e90d75008ac3d5e64
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-06-12 21:05:32 +0200

    [ljs]: Fix broken test

--- djs_ljs/smoke.test.ts
+++ djs_ljs/smoke.test.ts
@@ -1,17 +1,19 @@
 import { test } from "vitest"
 import { parse_source_file } from "djs_parser"
 import assert from "node:assert/strict"
+import { prettify_diagnostics } from "../djs_ast/Diagnostic.ts"
 
-test("smoke test", () => {
-  const source_file = parse_source_file(
-    "test/test.js",
-    `
-       extern function puts(s: *const u8): void
+test("smoke test", async () => {
+  const source = `
+     extern function puts(s: *const u8): void
 
-       export function main(): void {
-          puts(c"Hello, world!")
-       }
-    `,
+     export function main(): void {
+        puts(c\`Hello, world!\`)
+     }
+  `
+  const source_file = parse_source_file("test/test.ljs", source)
+  assert.equal(
+    await prettify_diagnostics("test/test.ljs", source_file.errors, source),
+    "",
   )
-  assert.deepEqual(source_file.errors, [])
 })
