0d3435c735b24a18fe4ea38a6d3fee7157f41bfb
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-06-13 16:25:28 +0200

    [djs_std]: Add a package for utilities

---
+++ djs_std/index.ts
@@ -0,0 +1,11 @@
+export type Prettify<T> = {
+  [K in keyof T]: T[K]
+} & unknown
+
+/**
+ * Takes a type T1 | T2, ... | Tn and returns
+ * Readonly<T1> | Readonly<T2> | ... | Readonly<Tn>
+ */
+export type ReadonlyUnion<T extends Record<string, unknown>> = T extends unknown
+  ? Prettify<Readonly<T>>
+  : never
