a71229620350226fa43818c2e984fcba0c52a015
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-06-28 11:39:15 +0200

    [ljs]: Use absolute paths

--- djs_ljs/FS.ts
+++ djs_ljs/FS.ts
@@ -1,33 +1,43 @@
-import assert from "node:assert"
 import { promises as node_fs } from "node:fs"
+import Path from "node:path"
 
 export interface FS {
   read_file(path: string, encoding: "utf-8"): Promise<string>
+  to_absolute(path: string): string
 }
+
+const real_fs: FS = {
+  read_file: async (path, encoding) => node_fs.readFile(path, encoding),
+  to_absolute: (path) => Path.resolve(path),
+}
+
 export const FS = {
-  real: {
-    read_file: async (path: string, encoding: "utf-8") =>
-      node_fs.readFile(path, encoding),
-  } satisfies FS,
+  real: real_fs,
   fake: fake_fs,
 }
 
 function fake_fs(_files: Record<string, string>): FS {
+  const cwd = "/"
   const files: Record<string, string> = Object.fromEntries(
     Object.entries(_files).map(([path, content]) => [
-      normalize_path(path),
+      to_absolute(path),
       content,
     ]),
   )
+  function to_absolute(path: string): string {
+    return Path.normalize(Path.join(cwd, path))
+  }
+
   return {
-    read_file: async (path_: string, encoding: "utf-8") => {
-      const path = normalize_path(path_)
-      assert.equal(encoding, "utf-8")
+    files,
+    read_file: async (path_) => {
+      const path = to_absolute(path_)
       if (!(path in files)) {
         throw new Error(`File not found: ${path}`)
       }
       return files[path]
     },
+    to_absolute,
   }
 }
 function normalize_path(path: string): string {
