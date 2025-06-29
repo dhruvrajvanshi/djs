e3ae2dff27f66f1ada513fc2c62855eb0ef616e4
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-06-29 13:22:15 +0200

    [ljs]: Improve pretty printing

--- djs_ljs/SourceFiles.ts
+++ djs_ljs/SourceFiles.ts
@@ -1,30 +1,4 @@
 import type { SourceFile } from "djs_ast"
-import type { FS } from "./FS.ts"
 import { PathMap } from "./PathMap.ts"
 
-export class SourceFiles {
-  #files: PathMap<SourceFile>
-
-  constructor(fs: FS) {
-    this.#files = new PathMap<SourceFile>(fs)
-  }
-
-  get(path: string): SourceFile | undefined {
-    return this.#files.get(path)
-  }
-  set(path: string, source_file: SourceFile): void {
-    this.#files.set(path, source_file)
-  }
-  entries(): IterableIterator<[string, SourceFile]> {
-    return this.#files.entries()
-  }
-  values(): IterableIterator<SourceFile> {
-    return this.#files.values()
-  }
-  get size(): number {
-    return this.#files.size
-  }
-  toString(): string {
-    return this.#files.toString()
-  }
-}
+export type SourceFiles = PathMap<SourceFile>
