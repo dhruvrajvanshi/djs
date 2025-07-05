7333f4482f858006c4baa734e6022b4437f46851
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-07-05 14:31:07 +0200

    WIP

--- djs_ljs/typecheck.ts
+++ djs_ljs/typecheck.ts
@@ -1,4 +1,4 @@
-import type { Expr } from "djs_ast"
+import { ASTVisitorBase, type Expr } from "djs_ast"
 import { Diagnostics } from "./diagnostics.ts"
 import type { FS } from "./FS.ts"
 import type { SourceFiles } from "./SourceFiles.ts"
@@ -14,6 +14,33 @@ export function typecheck(source_files: SourceFiles, fs: FS): TypeCheckResult {
     diagnostics,
     types: new Map<Expr, Type>(),
   }
+  const typecheck_visitor = new TypecheckVisitor()
+  typecheck_visitor.visit_source_files(source_files)
 
   return result
+}
+
+class TypecheckVisitor extends ASTVisitorBase {
+  private readonly visited_source_files: Set<string> = new Set()
+  public readonly types: Map<Expr, Type> = new Map()
+
+  visit_source_files(source_files: SourceFiles): void {
+    for (const source_file of source_files.values()) {
+      if (this.visited_source_files.has(source_file.path)) {
+        continue
+      }
+      this.visited_source_files.add(source_file.path)
+      this.visit_source_file(source_file)
+    }
+  }
+
+  override visit_expr(expr: Expr): void {
+    const existing = this.types.get(expr)
+    if (existing) return
+
+    switch (expr.kind) {
+      default:
+        super.visit_expr(expr)
+    }
+  }
 }
