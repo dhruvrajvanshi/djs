769e435c198a9695b6d49b24dbaca4a64ce4a571
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-07-16 17:56:18 +0200

    [ljs]: Partial work on the typechecker

--- djs_ljs/typecheck.ts
+++ djs_ljs/typecheck.ts
@@ -1,45 +1,85 @@
-import type { Expr, Ident, SourceFile } from "djs_ast"
+import type { Expr, SourceFile, Stmt } from "djs_ast"
 import type { SourceFiles } from "./SourceFiles.ts"
 import type { Type } from "./type.ts"
-import type { PathMap } from "./PathMap.ts"
-import type {
-  TypeDeclExcludingKind,
-  ValueDeclExcludingKind,
-} from "./SymbolTable.ts"
-import assert from "node:assert"
 import { Diagnostics } from "./diagnostics.ts"
+import { todo } from "djs_std"
 
 export interface TypecheckResult {
   types: Map<Expr, Type>
   diagnostics: Diagnostics
+  trace: Trace
 }
 
-export function typecheck(
-  source_files: SourceFiles,
-  value_bindings: PathMap<
-    Map<Ident, ValueDeclExcludingKind<"Import" | "ImportStarAs">>
-  >,
-  type_bindings: PathMap<
-    Map<Ident, TypeDeclExcludingKind<"Import" | "ImportStarAs">>
-  >,
-): TypecheckResult {
+interface TraceNode {
+  name: string
+}
+class Trace {
+  root: TraceNode = { name: "root" }
+  current: TraceNode = this.root
+  nodes = new Set<TraceNode>([this.root])
+  edges: [from: TraceNode, to: TraceNode][] = []
+
+  add(name: string) {
+    const node: TraceNode = { name }
+    this.nodes.add(node)
+    this.edges.push([this.current, node])
+    const old_current = this.current
+    this.current = node
+    const self = this
+    return {
+      [Symbol.dispose]() {
+        self.current = old_current
+      },
+    }
+  }
+}
+
+export function typecheck(source_files: SourceFiles): TypecheckResult {
   const diagnostics = new Diagnostics(source_files.fs)
   const types = new Map<Expr, Type>()
-  for (const source_file of source_files.values()) {
-    const source_file_values = value_bindings.get(source_file.path)
-    const source_file_types = type_bindings.get(source_file.path)
-    assert(source_file_values)
-    assert(source_file_types)
-    typecheck_source_file(source_file, source_file_values, source_file_types)
+  const trace = new Trace()
+
+  for (const file of source_files.values()) {
+    check_source_file(file)
   }
+
   return {
     diagnostics,
     types,
+    trace,
+  }
+
+  function check_source_file(file: SourceFile): void {
+    using _ = trace.add(`check_source_file(${file.path})`)
+    for (const stmt of file.stmts) {
+      check_stmt(stmt)
+    }
+  }
+  function check_stmt(stmt: Stmt): void {
+    using _ = trace.add(`check_stmt(${short_stmt_name(stmt)})`)
+    switch (stmt.kind) {
+      case "Import":
+        return check_import_stmt(stmt)
+      case "ImportStarAs":
+        return check_import_star_as_stmt(stmt)
+      case "TypeAlias":
+        return check_type_alias_stmt(stmt)
+      default:
+        todo(stmt.kind)
+    }
+  }
+
+  function check_import_stmt(stmt: Stmt): void {
+    using _ = trace.add(`check_import_stmt`)
+  }
+  function check_import_star_as_stmt(stmt: Stmt): void {
+    using _ = trace.add(`check_import_star_as_stmt`)
+  }
+  function check_type_alias_stmt(stmt: Stmt): void {
+    using _ = trace.add(`check_type_alias_stmt`)
   }
 }
 
-function typecheck_source_file(
-  source_file: SourceFile,
-  values: Map<Ident, ValueDeclExcludingKind<"Import" | "ImportStarAs">>,
-  types: Map<Ident, TypeDeclExcludingKind<"Import" | "ImportStarAs">>,
-): void {}
+function short_stmt_name(stmt: Stmt): string {
+  return stmt.kind
+}
