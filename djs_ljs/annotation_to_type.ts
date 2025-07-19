cd4b96f4b5f1288cf078f42c507a3a76b8358d3a
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-07-19 12:20:19 +0200

    [ljs]: Typechecker infrastructure
    Added a facility for tracing the typechecker operations and emit
    it as a graph. This helps with visualizing the dependency graphs
    implicit in the typechecker and hopefully will be a useful tool
    for debugging

---
+++ djs_ljs/annotation_to_type.ts
@@ -0,0 +1,30 @@
+import {
+  type_annotation_to_sexpr,
+  type Ident,
+  type TypeAnnotation,
+} from "djs_ast"
+import { Type } from "./type.ts"
+import { todo } from "djs_std"
+
+export function annotation_to_type(
+  env: (name: string | readonly [Ident, ...Ident[]]) => Type,
+  annotation: TypeAnnotation,
+): Type {
+  switch (annotation.kind) {
+    case "Ident":
+      switch (annotation.ident.text) {
+        case "u32":
+          return Type.u32
+        default:
+          return env(annotation.ident.text)
+      }
+    case "LJSConstPtr":
+      return Type.Ptr(annotation_to_type(env, annotation.to))
+    case "LJSPtr":
+      return Type.MutPtr(annotation_to_type(env, annotation.to))
+    case "Qualified":
+      return env([annotation.head, ...annotation.tail])
+    default:
+      return todo(annotation.kind)
+  }
+}
--- djs_ljs/typecheck.ts
+++ djs_ljs/typecheck.ts
@@ -1,37 +1,25 @@
-import type { Expr, SourceFile, Stmt } from "djs_ast"
+import {
+  expr_to_sexpr,
+  sexpr_to_string,
+  type Expr,
+  type FuncStmt,
+  type SourceFile,
+  type Stmt,
+  type TypeAnnotation,
+  type VarDeclStmt,
+} from "djs_ast"
 import type { SourceFiles } from "./SourceFiles.ts"
-import type { Type } from "./type.ts"
+import { Type } from "./type.ts"
 import { Diagnostics } from "./diagnostics.ts"
 import { todo } from "djs_std"
+import { flatten_var_decl } from "./flatten_var_decl.ts"
+import { annotation_to_type } from "./annotation_to_type.ts"
+import { Trace } from "./Trace.ts"
 
 export interface TypecheckResult {
   types: Map<Expr, Type>
   diagnostics: Diagnostics
   trace: Trace
-}
-
-interface TraceNode {
-  name: string
-}
-class Trace {
-  root: TraceNode = { name: "root" }
-  current: TraceNode = this.root
-  nodes = new Set<TraceNode>([this.root])
-  edges: [from: TraceNode, to: TraceNode][] = []
-
-  add(name: string) {
-    const node: TraceNode = { name }
-    this.nodes.add(node)
-    this.edges.push([this.current, node])
-    const old_current = this.current
-    this.current = node
-    const self = this
-    return {
-      [Symbol.dispose]() {
-        self.current = old_current
-      },
-    }
-  }
 }
 
 export function typecheck(source_files: SourceFiles): TypecheckResult {
@@ -50,33 +38,118 @@ export function typecheck(source_files: SourceFiles): TypecheckResult {
   }
 
   function check_source_file(file: SourceFile): void {
-    using _ = trace.add(`check_source_file(${file.path})`)
+    using _ = trace.add(`check_source_file\n${file.path}`)
     for (const stmt of file.stmts) {
-      check_stmt(stmt)
+      check_stmt(file, stmt)
     }
   }
-  function check_stmt(stmt: Stmt): void {
-    using _ = trace.add(`check_stmt(${short_stmt_name(stmt)})`)
+  function check_stmt(source_file: SourceFile, stmt: Stmt): void {
+    using _ = trace.add(`check_stmt\n${source_file.path}:${stmt.span.start}`)
     switch (stmt.kind) {
       case "Import":
-        return check_import_stmt(stmt)
+        return check_import_stmt(source_file, stmt)
       case "ImportStarAs":
-        return check_import_star_as_stmt(stmt)
+        return check_import_star_as_stmt(source_file, stmt)
       case "TypeAlias":
-        return check_type_alias_stmt(stmt)
+        return check_type_alias_stmt(source_file, stmt)
+      case "Func":
+        return check_func_stmt(source_file, stmt)
+      case "VarDecl":
+        return check_var_decl_stmt(source_file, stmt)
+      case "LJSExternFunction":
+        return check_ljs_extern_function_stmt(source_file, stmt)
+      case "Expr":
+        infer_expr(source_file, stmt.expr)
+        return
       default:
         todo(stmt.kind)
     }
   }
 
-  function check_import_stmt(stmt: Stmt): void {
-    using _ = trace.add(`check_import_stmt`)
+  function check_import_stmt(source_file: SourceFile, stmt: Stmt): void {
+    using _ = trace.add(
+      `check_import_stmt\n${source_file.path}:${stmt.span.start}`,
+    )
   }
-  function check_import_star_as_stmt(stmt: Stmt): void {
-    using _ = trace.add(`check_import_star_as_stmt`)
+  function check_import_star_as_stmt(
+    source_file: SourceFile,
+    stmt: Stmt,
+  ): void {
+    using _ = trace.add(
+      `check_import_star_as_stmt\n${source_file.path}:${stmt.span.start}}`,
+    )
   }
-  function check_type_alias_stmt(stmt: Stmt): void {
-    using _ = trace.add(`check_type_alias_stmt`)
+  function check_type_alias_stmt(source_file: SourceFile, stmt: Stmt): void {
+    using _ = trace.add(
+      `check_type_alias_stmt\n${source_file.path}:${stmt.span.start}}`,
+    )
+  }
+  function check_func_stmt(
+    source_file: SourceFile,
+    { func, span }: FuncStmt,
+  ): void {
+    using _ = trace.add(`check_func_stmt\n${source_file.path}:${span.start}`)
+    for (const stmt of func.body.stmts) {
+      check_stmt(source_file, stmt)
+    }
+  }
+  function check_var_decl_stmt(
+    source_file: SourceFile,
+    stmt: VarDeclStmt,
+  ): void {
+    using _ = trace.add(
+      `check_var_decl_stmt\n${source_file.path}:${stmt.span.start}`,
+      short_stmt_name(stmt),
+    )
+    for (const decl of flatten_var_decl(stmt)) {
+      const annotation = decl.type_annotation
+      let type: Type | null = null
+      if (annotation) {
+        type = check_type_annotation(annotation)
+      }
+      if (decl.init) {
+        if (type) {
+          check_expr(source_file, decl.init, type)
+        } else {
+          infer_expr(source_file, decl.init)
+        }
+      }
+    }
+  }
+
+  function check_ljs_extern_function_stmt(
+    source_file: SourceFile,
+    stmt: Stmt,
+  ): void {
+    using _ = trace.add(
+      `check_ljs_extern_function_stmt\n${source_file.path}:${stmt.span.start}}`,
+    )
+  }
+
+  function check_expr(
+    source_file: SourceFile,
+    expr: Expr,
+    expected_type: Type,
+  ): void {
+    using _ = trace.add(
+      `check_expr\n${source_file.path}:${expr.span.start}`,
+      sexpr_to_string(expr_to_sexpr(expr)),
+    )
+
+    // TODO
+    infer_expr(source_file, expr)
+  }
+  function infer_expr(source_file: SourceFile, expr: Expr): Type {
+    using _ = trace.add(
+      `infer_expr\n${source_file.path}:${expr.span.start}`,
+      sexpr_to_string(expr_to_sexpr(expr)),
+    )
+    return Type.Error("Unimplemented: infer_expr for " + expr.kind)
+  }
+
+  function check_type_annotation(annotation: TypeAnnotation): Type {
+    using _ = trace.add(`check_type_annotation(${annotation})`)
+    return annotation_to_type((v) => Type.Error("Unbound type" + v), annotation)
   }
 }
 
