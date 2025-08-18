8239519e51ed897045f484a7ffa81d76dab78c47
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-08-18 14:12:24 +0200

    Update the test runner for verifying files with diagnostics

--- djs_ljs/main.test.ts
+++ djs_ljs/main.test.ts
@@ -1,26 +1,36 @@
-import { test } from "vitest"
+import { test, expect } from "vitest"
 import { mkdir, readdir, readFile } from "node:fs/promises"
 import { main } from "./main.ts"
 import assert from "node:assert/strict"
 import { existsSync } from "node:fs"
-import { exec, spawn } from "node:child_process"
+import { spawn } from "node:child_process"
 
 const files = (await readdir("./tests")).filter((it) => it.endsWith(".ljs"))
 
 test.each(files)("%s", async (file) => {
-  const expected_stdout = await readFile(
-    `./tests/${file.replace(/\.ljs$/, "")}.stdout`,
-    "utf-8",
-  )
   await mkdir(`./test-output`, { recursive: true })
   const output = `./test-output/${file.replace(/\.ljs$/, "")}`
+  let diagnostics = ""
 
-  await main({
-    positionals: [`./tests/${file}`],
-    values: {
-      output: output,
+  await main(
+    {
+      positionals: [`./tests/${file}`],
+      values: {
+        output: output,
+        "no-colors": true,
+      },
     },
-  })
+    {
+      show_diagnostics: (d) => (diagnostics += d),
+      error_exit: () => {},
+    },
+  )
+  if (diagnostics) {
+    await expect(diagnostics).toMatchFileSnapshot(
+      `./tests/${file.replace(/\.ljs$/, "")}.diagnostics`,
+    )
+    return
+  }
   assert(existsSync(output))
   const result = await new Promise<{
     stdout: string
@@ -33,14 +43,12 @@ test.each(files)("%s", async (file) => {
     child.stdout.setEncoding("utf-8")
     child.stderr.setEncoding("utf-8")
     child.stdout.on("data", (data) => {
-      console.log("Stdout", data.toString())
       stdout += data.toString()
     })
     child.stderr.on("data", (data) => {
       stderr += data.toString()
     })
     child.on("exit", (code) => {
-      console.log("Exit code", code, stdout)
       if (code !== 0) {
         reject(new Error("Process exited with code " + code + "\n" + stderr))
       } else {
@@ -48,8 +56,10 @@ test.each(files)("%s", async (file) => {
       }
     })
   })
-  console.log(result)
   assert.equal(result.code, 0)
   assert.equal(result.stderr, "")
-  assert.equal(result.stdout, expected_stdout)
+
+  await expect(result.stdout).toMatchFileSnapshot(
+    `./tests/${file.replace(/\.ljs$/, "")}.stdout`,
+  )
 })
