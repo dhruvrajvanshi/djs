c42ce20537cb6cc33b9f15752c11cad4f0361758
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-10-17 17:18:41 +0200

    Fix test

--- djs_ljs/tests/link_c.c
+++ djs_ljs/tests/link_c.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 struct Box {
-    const char* message;
+  const char *message;
 };
 
-struct Box* box_new() {
-    struct Box* box= malloc(sizeof(struct Box));
-    box->message = "PASS";
-    return box;
+struct Box *box_new() {
+  struct Box *box = malloc(sizeof(struct Box));
+  box->message = "PASS";
+  return box;
 }
 
-void box_free(struct Box* box) {
-    free(box);
-}
+const char *box_message(const struct Box *box) { return box->message; }
+
+void box_free(struct Box *box) { free(box); }
